7038367091e349596d6a50ba3d7a4b12410d431c
elijulian <eliezer.julian@sapiens.com>
2018-03-05 15:12:21 +0200

    refactoring

--- src/main/java/CreateGateway.java
+++ src/main/java/CreateGateway.java
@@ -1,13 +1,10 @@
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.apigateway.AmazonApiGateway;
 import com.amazonaws.services.apigateway.AmazonApiGatewayClientBuilder;
-import com.amazonaws.services.apigateway.model.ContentHandlingStrategy;
+import com.amazonaws.services.apigateway.model.CreateDeploymentRequest;
+import com.amazonaws.services.apigateway.model.CreateModelRequest;
 import com.amazonaws.services.apigateway.model.CreateResourceRequest;
-import com.amazonaws.services.apigateway.model.CreateResourceResult;
-import com.amazonaws.services.apigateway.model.CreateRestApiRequest;
-import com.amazonaws.services.apigateway.model.CreateRestApiResult;
 import com.amazonaws.services.apigateway.model.GetResourcesRequest;
-import com.amazonaws.services.apigateway.model.GetResourcesResult;
 import com.amazonaws.services.apigateway.model.GetRestApisRequest;
 import com.amazonaws.services.apigateway.model.GetRestApisResult;
 import com.amazonaws.services.apigateway.model.IntegrationType;
@@ -15,95 +12,143 @@ import com.amazonaws.services.apigateway.model.PutIntegrationRequest;
 import com.amazonaws.services.apigateway.model.PutIntegrationResponseRequest;
 import com.amazonaws.services.apigateway.model.PutMethodRequest;
 import com.amazonaws.services.apigateway.model.PutMethodResponseRequest;
-import com.amazonaws.services.apigateway.model.PutMethodResult;
 import com.amazonaws.services.apigateway.model.Resource;
 import com.amazonaws.services.apigateway.model.RestApi;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.FunctionConfiguration;
+import com.amazonaws.services.lambda.model.GetFunctionRequest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class CreateGateway {
 
+    public static final String SECURITY_PROFILE_NAME = "default";
+    public static final String REGION = "eu-west-1";
+    public static final String DECISION_REST_API_NAME = "Decision";
+    public static final String DECISION_LAMBDA_CREDENTIALS = "arn:aws:iam::256265920797:role/lambda_basic_execution";
+    public static final String URI_TEMPLATE = "arn:aws:apigateway:eu-west-1:lambda:path/2015-03-31/functions/%s/invocations";
+
     public static void main(String[] args) {
 
-        AmazonApiGateway gateway = AmazonApiGatewayClientBuilder.standard()
-                .withCredentials(new ProfileCredentialsProvider("default")).withRegion("eu-west-1").build();
+        String decisionName = "math";
 
-        createRestApi(gateway);
+        AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider(SECURITY_PROFILE_NAME)).withRegion(REGION).build();
+        FunctionConfiguration decisionFunction = awsLambda.getFunction(new GetFunctionRequest().withFunctionName(decisionName)).getConfiguration();
+
+        AmazonApiGateway gateway = AmazonApiGatewayClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider(SECURITY_PROFILE_NAME)).withRegion(REGION).build();
 
         GetRestApisResult restApis = gateway.getRestApis(new GetRestApisRequest());
-        RestApi decision = restApis.getItems().stream().filter(item -> item.getName().equals("Decision")).findFirst()
+        RestApi decisionRestApi = restApis.getItems().stream()
+                .filter(item -> item.getName().equals(DECISION_REST_API_NAME)).findFirst()
                 .get();
 
-        GetResourcesResult resources = gateway.getResources(new GetResourcesRequest().withRestApiId(decision.getId()));
+        List<Resource> resources = gateway
+                .getResources(new GetResourcesRequest().withRestApiId(decisionRestApi.getId())).getItems();
+        Resource rootResource = resources.stream().filter(resource -> resource.getPath().equals("/")).findFirst().orElse(null);
 
-        createResource(gateway, decision, resources);
+        String decisionNameResourceId = createResource(gateway, decisionRestApi.getId(), rootResource.getId(), decisionName);
 
-        resources = gateway.getResources(new GetResourcesRequest().withRestApiId(decision.getId()));
-        Resource decisionResource = resources.getItems().stream().filter(item -> item.getPath().contains("decision"))
-                .findFirst().get();
+        createModel(gateway, decisionRestApi, ()->getRequestModel(decisionName), "Input");
+        createModel(gateway, decisionRestApi, ()->getResponseModel(decisionName), "Output");
 
-        createMethodForResource(gateway, decision, resources);
-        createMethodResponse(gateway, decision, decisionResource);
+        createMethodForResource(gateway, decisionRestApi.getId(), decisionNameResourceId);
+        createMethodResponse(gateway, decisionRestApi.getId(), decisionNameResourceId);
 
-        createIntegration(gateway, decision, decisionResource);
-        createIntegrationResponse(gateway, decision, decisionResource);
+        createIntegration(gateway, decisionRestApi.getId(), decisionNameResourceId, decisionFunction.getFunctionArn());
+        createIntegrationResponse(gateway, decisionRestApi.getId(), decisionNameResourceId);
+
+        gateway.createDeployment(new CreateDeploymentRequest()
+                                         .withRestApiId(decisionRestApi.getId())
+                                 .withStageName("PROD")
+                                 .withStageDescription("bla bla bla"));
     }
 
-    private static void createMethodResponse(AmazonApiGateway gateway, RestApi decision, Resource decisionResource) {
-        gateway.putMethodResponse(new PutMethodResponseRequest().withHttpMethod("POST").withRestApiId(decision.getId())
-                                          .withResourceId(decisionResource.getId()).withStatusCode("200"));
+    private static String createModel(AmazonApiGateway gateway, RestApi decisionRestApi, Supplier<String> getModel, String modelName) {
+        return gateway.createModel(new CreateModelRequest()
+                            .withSchema(getModel.get())
+                            .withContentType("application/json")
+                            .withName(modelName)
+                            .withDescription("bla bla bla")
+                            .withRestApiId(decisionRestApi.getId())).getId();
     }
 
-    private static void createIntegration(AmazonApiGateway gateway, RestApi decision, Resource decisionResource) {
+    private static void createMethodResponse(AmazonApiGateway gateway, String decisionRestApiId, String decisionResourceId) {
+
+        Map<String, String> modelName = new HashMap<String, String>(){{
+            put("application/json", "Output");
+        }};
+        gateway.putMethodResponse(new PutMethodResponseRequest()
+                                          .withHttpMethod("POST")
+                                          .withResponseModels(modelName)
+                                          .withRestApiId(decisionRestApiId)
+                                          .withResourceId(decisionResourceId)
+                                          .withStatusCode("200"));
+    }
+
+    private static void createIntegration(AmazonApiGateway gateway,
+                                          String decisionRestApiId,
+                                          String decisionResourceId,
+                                          String lambdaArn) {
 
         gateway.putIntegration(new PutIntegrationRequest()
-                                       .withCredentials("arn:aws:iam::256265920797:role/lambda_basic_execution")
-                                       .withIntegrationHttpMethod("POST").withHttpMethod("POST")
-                                       .withType(IntegrationType.AWS).withRestApiId(decision.getId())
-                                       .withResourceId(decisionResource.getId())
-                                       .withPassthroughBehavior("WHEN_NO_MATCH").withUri(
-                        "arn:aws:apigateway:eu-west-1:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-west-1:256265920797:function:math/invocations"));
+                                       .withCredentials(DECISION_LAMBDA_CREDENTIALS)
+                                       .withIntegrationHttpMethod("POST")
+                                       .withHttpMethod("POST")
+                                       .withType(IntegrationType.AWS)
+                                       .withRestApiId(decisionRestApiId)
+                                       .withResourceId(decisionResourceId)
+                                       .withPassthroughBehavior("WHEN_NO_MATCH")
+                                       .withUri(String.format(URI_TEMPLATE, lambdaArn)));
 
     }
 
     private static void createIntegrationResponse(AmazonApiGateway gateway,
-                                                  RestApi decision,
-                                                  Resource decisionResource) {
+                                                  String decisionRestApiId,
+                                                  String decisionResourceId) {
 
-        gateway.putIntegrationResponse(new PutIntegrationResponseRequest().withRestApiId(decision.getId())
-                                               .withResourceId(decisionResource.getId())
-                                               .withContentHandling(ContentHandlingStrategy.CONVERT_TO_TEXT)
-                                               .withHttpMethod("POST").withStatusCode("200"));
+        gateway.putIntegrationResponse(new PutIntegrationResponseRequest().withRestApiId(decisionRestApiId)
+                                               .withResourceId(decisionResourceId)
+                                               .withHttpMethod("POST")
+                                               .withStatusCode("200"));
 
     }
 
     private static void createMethodForResource(AmazonApiGateway gateway,
-                                                RestApi decision,
-                                                GetResourcesResult resources) {
-        Map<String, Boolean> params = new HashMap<>();
-        params.put("method.request.path.test", true);
+                                                String decisionRestApiId,
+                                                String resourceId) {
 
-        PutMethodResult putMethodResult = gateway.putMethod(new PutMethodRequest()
-                                                                    .withResourceId(resources.getItems().stream()
-                                                                                            .filter(item -> item
-                                                                                                    .getPath()
-                                                                                                    .contains("decision"))
-                                                                                            .findFirst().get().getId())
-                                                                    .withRestApiId(decision.getId())
+        Map<String, String> modelName = new HashMap<String, String>(){{
+            put("application/json", "Input");
+        }};
+        gateway.putMethod(new PutMethodRequest().withResourceId(resourceId)
+                                                                    .withRestApiId(decisionRestApiId)
                                                                     .withAuthorizationType("NONE")
-                                                                    .withHttpMethod("POST")
-                                                                    .withRequestParameters(params));
+                                                                    .withRequestModels(modelName)
+                                                                    .withHttpMethod("POST"));
     }
 
-    private static void createResource(AmazonApiGateway gateway, RestApi decision, GetResourcesResult resources) {
-        CreateResourceResult createResourceResult = gateway
-                .createResource(new CreateResourceRequest().withParentId(resources.getItems().get(0).getId())
-                                        .withRestApiId(decision.getId()).withPathPart("decision"));
+    private static String createResource(AmazonApiGateway gateway,
+                                       String decisionRestApiId,
+                                       String rootResourceId,
+                                       String decisionName) {
+        return gateway.createResource(new CreateResourceRequest().withParentId(rootResourceId).withRestApiId(decisionRestApiId)
+                                       .withPathPart(decisionName)).getId();
     }
 
-    private static void createRestApi(AmazonApiGateway gateway) {
-        CreateRestApiResult result = gateway.createRestApi(new CreateRestApiRequest().withName("Decision")
-                                                                   .withDescription("A new Decision gateway"));
+    public static String getRequestModel(String decisionName){
+         return "{\n" + "    \"type\":\"object\",\n" + "    \"properties\":{\n" + "        \"a\":{\"type\":\"number\"},\n"
+                 + "        \"b\":{\"type\":\"number\"},\n" + "        \"op\":{\"type\":\"string\"}\n" + "    },\n"
+                 + "    \"title\":\"" + decisionName + "\"\n" + "}";
+    }
+
+    public static String getResponseModel(String decisionName){
+        return "{\n" + "    \"type\":\"object\",\n" + "    \"properties\":{\n" + "        \"c\":{\"type\":\"number\"}\n"
+                + "    },\n" + "    \"title\":\"" + decisionName +"\"\n" + "}";
     }
 }
